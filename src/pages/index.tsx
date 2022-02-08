81a63dbbcb26c2752882244adc023901629cdd5e
the-abe-train <abraham.train@gmail.com>
2022-02-08 12:22:03 -0500

    Configured Tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <h1 className="text-blue-200 underline">The Abe Train</h1>
+      <p className="text-red-500">Hello!</p>
+    </main>
+  );
+};
+
+export default IndexPage;
