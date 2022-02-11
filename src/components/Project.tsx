76628d712dc89348492971687878e7a29354bf53
the-abe-train <abraham.train@gmail.com>
2022-02-11 00:46:50 -0500

    Fixed project links

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -12,6 +12,15 @@ type Props = {
   repo: string;
 };
 
+// TODO TEST In Project, need more space between text and icon on mobile
+// TODO add dark theme (with context?)
+// TODO test on Lighthouse
+// TODO test on Safari
+
+const style = {
+  fontFamily: "Copse",
+};
+
 export default function Project({
   title,
   description,
@@ -22,9 +31,11 @@ export default function Project({
 }: Props) {
   const projectImage = getImage(image);
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center space-x-2">
       <div className="w-60 sm:w-56 mb-4 flex flex-col space-y-3">
-        <h3 className="text-2xl">{title}</h3>
+        <h3 className="text-2xl" style={style}>
+          {title}
+        </h3>
         <p>{description}</p>
         <div className="flex space-x-4">
           <a href={repo}>
@@ -35,8 +46,8 @@ export default function Project({
           </a>
         </div>
       </div>
-      <a href={link}>
-        <div className="h-full w-28 relative drop-shadow-lg ">
+      <a href={url}>
+        <div className="h-full w-28 relative drop-shadow-[0_0_5px_rgba(0,0,0,0.5)]">
           {projectImage && (
             <GatsbyImage
               className="absolute right-0 top-1/2 transform -translate-y-1/2"
